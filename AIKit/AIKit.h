872270682855ff4bd216d196f0159b3e5022afb3
Brad Hilton <brad.hilton.nw@gmail.com>
2018-03-30 10:16:27 -0600

    Initial Commit

---
+++ AIKit/AIKit.h
@@ -0,0 +1,19 @@
+//
+//  AIKit.h
+//  AIKit
+//
+//  Created by Bradley Hilton on 3/30/18.
+//  Copyright © 2018 AIKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AIKit.
+FOUNDATION_EXPORT double AIKitVersionNumber;
+
+//! Project version string for AIKit.
+FOUNDATION_EXPORT const unsigned char AIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AIKit/PublicHeader.h>
+
+
