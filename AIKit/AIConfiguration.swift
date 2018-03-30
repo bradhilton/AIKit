1ba505deb87c008a2780f7755d959359beb2917d
Brad Hilton <brad.hilton.nw@gmail.com>
2018-03-30 12:15:08 -0600

    Adding Configuration and Response

---
+++ AIKit/AIConfiguration.swift
@@ -0,0 +1,18 @@
+//
+//  AIConfiguration.swift
+//  AIKit
+//
+//  Created by Bradley Hilton on 3/30/18.
+//  Copyright © 2018 AIKit. All rights reserved.
+//
+
+public class AIConfiguration {
+ 
+    public init () {}
+    
+    
+    
+    
+    
+    
+}
---
+++ AIKit/AIResponse.swift
@@ -0,0 +1,11 @@
+//
+//  AIResponse.swift
+//  AIKit
+//
+//  Created by Bradley Hilton on 3/30/18.
+//  Copyright © 2018 AIKit. All rights reserved.
+//
+
+public enum AIResponse {
+    case text(String)
+}
