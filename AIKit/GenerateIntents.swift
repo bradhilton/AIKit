e2322a895bee22c6dba74400408ae269f138574a
Brad Hilton <brad.hilton.nw@gmail.com>
2018-03-31 09:50:24 -0600

    Generate Intents

---
+++ AIKit/GenerateIntents.swift
@@ -0,0 +1,13 @@
+//
+//  GenerateIntents.swift
+//  AIKit
+//
+//  Created by Bradley Hilton on 3/31/18.
+//  Copyright © 2018 AIKit. All rights reserved.
+//
+
+import Foundation
+
+let configuration = URLSessionConfiguration.default
+configuration.httpAdditionalHeaders = ["Authorization" : "Bearer NLGR5H26PCJR6HQQVAUOKVIWVMU4I55W"]
+let session = URLSession(configuration: configuration)
