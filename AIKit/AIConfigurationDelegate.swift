30341bb0fd08996c9de80646113aa26ba7f129dd
Brad Hilton <brad.hilton.nw@gmail.com>
2018-03-30 20:03:05 -0600

    Change to delegate

---
+++ AIKit/AIConfigurationDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AIConfigurationDelegate.swift
+//  AIKit
+//
+//  Created by Bradley Hilton on 3/30/18.
+//  Copyright © 2018 AIKit. All rights reserved.
+//
+
+protocol AIConfigurationDelegate : class {
+    func configuration(_ configuration: AIConfiguration, userInputUpdated input: String)
+    func configurationStartedLoadingResponse(_ configuration: AIConfiguration)
+    func configuration(_ configuration: AIConfiguration, didReceiveResponse response: AIResponse)
+}
